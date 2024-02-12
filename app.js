6e0e3861bc1cc63458fa8b55654e2f9cf8126f41
jpoots <90725697+jpoots@users.noreply.github.com>
2024-02-12 22:14:53 +0000

    card page, data rendering and db connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mysql = require('mysql2');
 
 // set up app
 const app = express();
@@ -8,6 +9,20 @@ const PORT = 3000;
 
 // css and images
 app.use(express.static(path.join(__dirname, "./public")));
+
+// establish db connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",         
+    password: "root",         
+    database: "trademon",
+    port: "8889"
+});
+
+db.connect((err)=> {
+    if(err) throw err;
+    console.log('database connected successfully');
+});
 
 // homepage
 app.get("/", (req, res) => {
@@ -26,7 +41,43 @@ app.get("/register", (req, res) => {
 
 // browse
 app.get("/browse", (req, res) => {
-    res.render("browse");
+    const rowSize = 5;
+
+    const example = {
+        name: "Roselia",
+        image: "https://assets.tcgdex.net/en/swsh/swsh1/2/high.webp"
+    }
+    const data = [];
+
+    for (let i=0; i < 10; i++){
+        data.push(example);
+    }
+    
+    const dataToPush = []
+
+    // https://medium.com/@drdDavi/split-a-javascript-array-into-chunks-d90c90de3a2d
+    for (let i = 0; i < data.length; i += rowSize) {
+        const chunk = data.slice(i, i + rowSize);
+        dataToPush.push(chunk);
+    }
+    // end of reference
+
+    res.render("browse", {cards: dataToPush});
+});
+
+// card
+app.get("/card", (req, res) => {
+    cardData = [{
+        name: "Roselia",
+        set: "a set",
+        category: "a category",
+        evolves: "evolves",
+        stage: "1",
+        hp: 100,
+        illustrator: "Me",
+        image: "https://assets.tcgdex.net/en/swsh/swsh1/2/high.webp"
+    }]
+    res.render("card", {card : cardData});
 });
 
 app.listen(PORT, () => {
