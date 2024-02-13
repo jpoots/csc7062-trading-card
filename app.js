61acb074717b38ecca805abea1e1ec8ad409dfa4
jpoots <90725697+jpoots@users.noreply.github.com>
2024-02-13 18:13:35 +0000

    Updates to card page, more db work, search bar added, added price API call

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const pokemon = require("pokemontcgsdk");
 
 // set up app
 const app = express();
@@ -42,42 +43,69 @@ app.get("/register", (req, res) => {
 // browse
 app.get("/browse", (req, res) => {
     const rowSize = 5;
+    let data = [];
 
-    const example = {
-        name: "Roselia",
-        image: "https://assets.tcgdex.net/en/swsh/swsh1/2/high.webp"
-    }
-    const data = [];
+    let read = "SELECT card_id, name, image_url FROM card";
+    db.query(read,(err, result) => {
+        if(err) throw err;
 
-    for (let i=0; i < 10; i++){
-        data.push(example);
-    }
-    
-    const dataToPush = []
+        // https://medium.com/@drdDavi/split-a-javascript-array-into-chunks-d90c90de3a2d
+        for (let i = 0; i < result.length; i += rowSize) {
+            const chunk = result.slice(i, i + rowSize);
+            data.push(chunk);
+        }
+        // end of reference
 
-    // https://medium.com/@drdDavi/split-a-javascript-array-into-chunks-d90c90de3a2d
-    for (let i = 0; i < data.length; i += rowSize) {
-        const chunk = data.slice(i, i + rowSize);
-        dataToPush.push(chunk);
-    }
-    // end of reference
+        res.render("browse", {cards: data});
+    });
 
-    res.render("browse", {cards: dataToPush});
+
 });
 
 // card
-app.get("/card", (req, res) => {
-    cardData = [{
-        name: "Roselia",
-        set: "a set",
-        category: "a category",
-        evolves: "evolves",
-        stage: "1",
-        hp: 100,
-        illustrator: "Me",
-        image: "https://assets.tcgdex.net/en/swsh/swsh1/2/high.webp"
-    }]
-    res.render("card", {card : cardData});
+app.get("/card/:cardId", (req, res) => {
+    // get card id from the req
+    let cardID = req.params.cardId;
+    let read = `SELECT * FROM card WHERE card_id = ${cardID}`;
+
+    // get card data from 
+    db.query(read, async (err, result) => {
+        // error checking
+        if(err) throw err;
+        if (result.length === 0) return res.redirect("/browse");
+
+        // format card data and get price data
+        let card = result[0]
+        let response = await fetch(`https://api.pokemontcg.io/v2/cards/${card.tcg_id}`);
+        response = await response.json();
+        let price = response.data.cardmarket.prices.trendPrice;;
+        let priceURL = response.data.cardmarket.url
+
+        // prepare card data for rendering
+        cardData = {
+            name: card.name,
+            set: "a set",
+            expansion: "an expansion",
+            category: "Pokemon",
+            evolves: "evolves",
+            stage: "Basic",
+            hp: card.hp,
+            illustrator: "Me",
+            image: card.image_url,
+            type: ["https://static.tcgcollector.com/content/images/90/d7/49/90d74923dfb481342fb5cb6c78e5fc6f6a8992cbd72a127d78af726c412a1bdc.png"],
+            attack:[
+                {
+                    name: "Absorb",
+                    effect: "Heal 10 damage from this Pokémon.",
+                    damage: 10
+                }
+            ],
+            price: price,
+            priceURL: priceURL
+        }
+
+        res.render("card", {card : cardData});
+    });
 });
 
 app.listen(PORT, () => {
